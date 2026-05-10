72b2af2ca8f3f93810ba07606e9597609b5a012b
Tyler Forman <tforman0115@gmail.com>
2026-05-10 17:51:14 -0400

    Added new kanji set

---
+++ src/data/kanji/n5/kanji_big_to_left.tsx
@@ -0,0 +1,126 @@
+export default [
+    {
+        jp: '大',
+        en: 'big',
+    },
+    {
+        jp: '犬',
+        en: 'dog',
+    },
+    {
+        jp: '半',
+        en: 'half',
+    },
+    {
+        jp: '前',
+        en: 'in front',
+    },
+    {
+        jp: '来',
+        en: 'come',
+    },
+    {
+        jp: '川',
+        en: 'river',
+    },
+    {
+        jp: '山',
+        en: 'mountain',
+    },
+    {
+        jp: '出',
+        en: 'leave',
+    },
+    {
+        jp: '入',
+        en: 'enterance',
+    },
+    {
+        jp: '天',
+        en: 'heaven',
+    },
+    {
+        jp: '夕',
+        en: 'evening',
+    },
+    {
+        jp: '名',
+        en: 'name',
+    },
+    {
+        jp: '化',
+        en: 'change',
+    },
+    {
+        jp: '花',
+        en: 'flower',
+    },
+    {
+        jp: '竹 ',
+        en: 'bamboo',
+    },
+    {
+        jp: '早',
+        en: 'early',
+    },
+    {
+        jp: '草',
+        en: 'weed',
+    },
+    {
+        jp: '林',
+        en: 'grove',
+    },
+    {
+        jp: '森',
+        en: 'forest',
+    },
+    {
+        jp: '交',
+        en: 'mingle',
+    },
+    {
+        jp: '校',
+        en: 'school',
+    },
+    {
+        jp: '言',
+        en: 'word',
+    },
+    {
+        jp: '語',
+        en: 'language',
+    },
+    {
+        jp: '話',
+        en: 'story',
+    },
+    {
+        jp: '売',
+        en: 'sell',
+    },
+    {
+        jp: '読',
+        en: 'read',
+    },
+    {
+        jp: '本',
+        en: 'book',
+    },
+    {
+        jp: '文',
+        en: 'literary arts',
+    },
+    {
+        jp: '書',
+        en: 'write',
+    },
+    {
+        jp: '右',
+        en: 'right',
+    },
+    {
+        jp: '左',
+        en: 'left',
+    },
+];
